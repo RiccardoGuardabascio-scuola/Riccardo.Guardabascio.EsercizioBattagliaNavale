df39a1d5065025814724007aa6dfaf7557394234
PC <PC@INFO5-2561>
2024-04-19 09:24:45 +0200

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Riccardo.Guardabascio.EsercizioBattagliaNavale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
